e670b14cbb9e6d2cd75fdcc6e2842b2ed3a48aac
Kamal <kamalp.amrita@gmail.com>
2020-07-26 22:12:41 -0400

    Guy working in Texas

--- Cucumber/src/main/java/Cucumber_Automation/Cucumber/App.java
+++ Cucumber/src/main/java/Cucumber_Automation/Cucumber/App.java
@@ -11,6 +11,8 @@ public class App
         System.out.println( "Hello World! This is Kamal Palanisamy who will become a selfmade millionaire in 2022. God will bless Kamal" );
 	System.out.println( "Hello World! This is Kamal Palanisamy who will get $180,000 salary per year from year 2020 and he will work in United States of America till 2030 with
 				the help of God's blessing" );
-
+	System.out.println("Kamal will buy lot of apartment complexes that consist of 200 houses from 2020 to 2030");
+	System.out.println("Kamal will marry to a wonderful girl in 2021 and he will lead happy life with her until rest of his life 2070");
+	System.out.println("Barani will marry to a wonderful guy in 2021 and she will lead happy life with her until rest of his life 2070");
     }
 }
