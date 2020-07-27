58fdf8b9438d2ef0bbf4a31d8eac19120ef42dfd
Kamal <kamalp.amrita@gmail.com>
2020-07-26 22:03:40 -0400

    Commited by Guy working in Texas USA

--- Cucumber/src/main/java/Cucumber_Automation/Cucumber/App.java
+++ Cucumber/src/main/java/Cucumber_Automation/Cucumber/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Kamal Palanisamy who will become a selfmade millionaire in 2022" );
+        System.out.println( "Hello World! This is Kamal Palanisamy who will become a selfmade millionaire in 2022. God will bless Kamal" );
 	System.out.println( "Hello World! This is Kamal Palanisamy who will get $180,000 salary per year from year 2020 and he will work in United States of America till 2030 with
 				the help of God's blessing" );
 
